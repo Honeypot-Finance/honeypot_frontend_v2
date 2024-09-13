1f7cb1c887f6dbed0a966995f1ee2d5de847fc97
zongzheng <304389312@qq.com>
2024-09-13 19:12:14 +0800

    add verison compare
    Signed-off-by: zongzheng <304389312@qq.com>

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -11,6 +11,10 @@ import { cn } from "@/lib/tailwindcss";
 import NotConnetctedDisplay from "../NotConnetctedDisplay/NotConnetctedDisplay";
 import ConfettiComponent from "../atoms/Confetti/Confetti";
 import PopOverModal from "../PopOverModal/PopOverModal";
+import { trpcClient } from "@/lib/trpc";
+import { popmodal } from "@/services/popmodal";
+import Link from "next/link";
+import { metadata } from "@/config/metadata";
 
 export const Layout = ({
   children,
@@ -22,6 +26,30 @@ export const Layout = ({
   const router = useRouter();
   const { chainId } = useAccount();
   const currentChain = chainId ? networksMap[chainId] : null;
+
+  useEffect(() => {
+    trpcClient.metadata.getServerMetadata.query().then((res) => {
+      if (res.latest_version === metadata.version || process.env.NODE_ENV === 'development' ) return;
+      popmodal.openModal({
+        content: (
+          <div className="min-h-[300px] line-[24px]">
+            <p className="text-center  font-bold text-[30px]">Announcement</p>
+            <h1 className="mt-[24px]">This version is outdated, please check our newest link:&nbsp;    <a
+                className="hover:text-orange-500 transition-all underline"
+                href={res.latest_site}
+              >
+                {res.latest_site}.
+              </a></h1>
+              <p>
+                Meanwhile, please migrate your assets to our new version
+              </p>
+
+          </div>
+        ),
+      });
+    });
+  }, []);
+
   return (
     <div className={cn("flex flex-col min-h-screen overflow-auto", className)}>
       <ConfettiComponent />
