16db35444e4ae7cfd96af41fd0df2b70b9d58fa4
rabbitblood <qz154772610@gmail.com>
2024-10-31 14:34:13 -0700

    update migrate to new version text

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -15,6 +15,7 @@ import { trpcClient } from "@/lib/trpc";
 import { popmodal } from "@/services/popmodal";
 import Link from "next/link";
 import { metadata } from "@/config/metadata";
+import { toast } from "react-toastify";
 
 export const Layout = ({
   children,
@@ -34,31 +35,52 @@ export const Layout = ({
         // ||process.env.NODE_ENV === "development"
       )
         return;
-      popmodal.openModal({
-        content: (
-          <div className="min-h-[300px] line-[24px]">
-            <p className="text-center  font-bold text-[30px]">Announcement</p>
-            <h1 className="mt-[24px]">
-              This version is outdated, please check our newest link:&nbsp;{" "}
-              <a
-                className="hover:text-orange-500 transition-all underline"
-                href={res.latest_site}
-              >
-                {res.latest_site}.
-              </a>
-            </h1>
-            <p>Meanwhile, please migrate your assets to our new version</p>
-            <br />
-            <h2>how:</h2>
-            <ol className="mb-5">
-              <li className="pl-5">1. go to my pools in old link</li>
-              <li className="pl-5">2. remove liquidity</li>
-              <li className="pl-5">3. go to new link</li>
-              <li className="pl-5">4. add liquidity</li>
-            </ol>
-          </div>
-        ),
-      });
+      // popmodal.openModal({
+      //   content: (
+      //     <div className="min-h-[300px] line-[24px]">
+      //       <p className="text-center  font-bold text-[30px]">Announcement</p>
+      //       <h1 className="mt-[24px]">
+      //         This version is outdated, please check our newest link:&nbsp;{" "}
+      //         <a
+      //           className="hover:text-orange-500 transition-all underline"
+      //           href={res.latest_site}
+      //         >
+      //           {res.latest_site}.
+      //         </a>
+      //       </h1>
+      //       <p>Meanwhile, please migrate your assets to our new version</p>
+      //       <br />
+      //       <h2>how:</h2>
+      //       <ol className="mb-5">
+      //         <li className="pl-5">1. go to my pools in old link</li>
+      //         <li className="pl-5">2. remove liquidity</li>
+      //         <li className="pl-5">3. go to new link</li>
+      //         <li className="pl-5">4. add liquidity</li>
+      //       </ol>
+      //     </div>
+      //   ),
+      // });
+
+      toast.info(
+        <div>
+          <h3>
+            You may migrate assets to new version of testnet dex:{" "}
+            <a
+              className="hover:text-orange-500 transition-all underline"
+              href={res.latest_site}
+            >
+              {res.latest_site}.
+            </a>
+          </h3>
+          <h3>how:</h3>
+          <ol className="mb-5">
+            <li className="pl-5">1. go to my pools in old link</li>
+            <li className="pl-5">2. remove liquidity</li>
+            <li className="pl-5">3. go to new link</li>
+            <li className="pl-5">4. add liquidity</li>
+          </ol>
+        </div>
+      );
     });
   }, []);
 
