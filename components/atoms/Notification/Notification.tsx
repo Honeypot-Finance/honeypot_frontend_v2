d9d61d075f6e8441380764a21ef9b8b9b033de77
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-02-24 17:36:34 -0800

    fix notification not showing

--- components/atoms/Notification/Notification.tsx
+++ components/atoms/Notification/Notification.tsx
@@ -6,7 +6,10 @@ type NotificationProps = {
   notify: boolean;
   dotSize?: number;
   dotColor?: string;
-  className?: string;
+  classNames?: {
+    container: string;
+    dot: string;
+  };
 };
 
 export const Notification = observer(
@@ -15,14 +18,15 @@ export const Notification = observer(
     notify,
     dotSize = 10,
     dotColor = "#ff0000", // red
-    className,
+    classNames,
   }: NotificationProps) => {
     return (
-      <div className={cn("relative overflow-visible", className)}>
+      <div className={cn("relative overflow-visible", classNames?.container)}>
         {notify && (
           <div
             className={cn(
-              `absolute top-0 right-0 translate-x-[50%] translate-y-[-50%] w-[${dotSize}px] h-[${dotSize}px] bg-[${dotColor}] rounded-full z-10`
+              `absolute top-0 right-0 translate-x-[50%] translate-y-[-50%] w-[10px] h-[10px] bg-red-500 rounded-full z-10`,
+              classNames?.dot
             )}
           />
         )}
