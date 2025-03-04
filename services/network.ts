70530b9209d1ece4a27fdc2f4482f69e185143cc
Hongming Wang <hongmingwangrabbit@gmail.com>
2025-03-03 16:17:15 -0800

    update validated tokens

--- services/network.ts
+++ services/network.ts
@@ -101,6 +101,7 @@ export const berachainNetwork = new Network({
     decimals: 18,
     isNative: true,
     logoURI: "/images/icons/tokens/wbera-token-icon.png",
+    isPopular: true,
   },
   raisedTokenData: [
     // {
@@ -198,16 +199,15 @@ export const berachainNetwork = new Network({
     poolBlacklist: [],
     memeBlacklist: [],
   },
+
   validatedTokensInfo: {
     //when adding a new token, make sure to add the address as lowercase
-
     "0x6969696969696969696969696969696969696969": {
       name: "Wrapped Bera",
       symbol: "WBERA",
       decimals: 18,
       logoURI: "/images/icons/tokens/wbera-token-icon.png",
       isRouterToken: true,
-      isPopular: true,
     },
     "0xfcbd14dc51f0a4d49d5e53c2e0950e0bc26d0dce": {
       name: "Honey",
@@ -218,12 +218,11 @@ export const berachainNetwork = new Network({
       isPopular: true,
     },
     "0x9b37d542114070518a44e200fdcd8e4be737297f": {
-      name: "HPOT",
+      name: "Honeypot Finance",
       symbol: "HPOT",
       decimals: 18,
       logoURI: "/images/icons/tokens/thpot-token-icon.jpg",
       isRouterToken: true,
-      isPopular: true,
     },
     "0x549943e04f40284185054145c6e4e9568c1d3241": {
       name: "USDC",
@@ -246,17 +245,92 @@ export const berachainNetwork = new Network({
       logoURI: "/images/icons/tokens/weth-token-icon.png",
       isRouterToken: true,
     },
-    "0x1cE0a25D13CE4d52071aE7e02Cf1F6606F4C79d3": {
+    "0x5c43a5fef2b056934478373a53d1cb08030fd382": {
+      name: "Berally Token",
+      symbol: "BRLY",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/berally.png",
+    },
+    "0x1ce0a25d13ce4d52071ae7e02cf1f6606f4c79d3": {
       name: "NECT",
       symbol: "NECT",
       decimals: 18,
-      logoURI: "/images/icons/tokens/nect-token-icon.png",
+      logoURI: "/images/icons/tokens/nect-token.jpg",
+      isPopular: true,
+    },
+    "0x467aa1bfa3dcc714f7c16b3d779200431f6a833b": {
+      name: "3BC",
+      symbol: "3BC",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/3bc.png",
     },
     "0xd77552d3849ab4d8c3b189a9582d0ba4c1f4f912": {
-      name: "WGBERA",
-      symbol: "WGBERA",
+      name: "wgBERA",
+      symbol: "wgBERA",
       decimals: 18,
-      logoURI: "/images/icons/tokens/wgbera-token-icon.png",
+      logoURI: "/images/icons/tokens/wgbera.png",
+      isPopular: true,
+    },
+    "0x779ded0c9e1022225f8e0630b35a9b54be713736": {
+      name: "USD₮0",
+      symbol: "USD₮0",
+      decimals: 6,
+      logoURI: "/images/icons/tokens/usdt-token-icon.png",
+    },
+    "0xbc665a196220043b738de189aef05250e2acc700": {
+      name: "Boyz",
+      symbol: "Boyz",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/boyz-token-icon.png",
+    },
+    "0x9b6761bf2397bb5a6624a856cc84a3a14dcd3fe5": {
+      name: "iBERA",
+      symbol: "iBERA",
+      decimals: 18,
+      logoURI: "https://infrared.finance/_next/static/media/ibera.a6d10126.svg",
+    },
+    "0x08a38caa631de329ff2dad1656ce789f31af3142": {
+      name: "YEET",
+      symbol: "YEET",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/yeet-token-icon.jpg",
+    },
+    "0x1f7210257fa157227d09449229a9266b0d581337": {
+      name: "Beramonium Coin",
+      symbol: "BERAMO",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/beramonium.png",
+    },
+    "0x331865bf2ea19e94bbf438cf4ee590cb6392e5a9": {
+      name: "Moola",
+      symbol: "MOOLA",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/moola.jpeg",
+    },
+    "0xa452810a4215fccc834ed241e6667f519b9856ec": {
+      name: "Berabot",
+      symbol: "BBOT",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/berabot.png",
+    },
+    "0xac03caba51e17c86c921e1f6cbfbdc91f8bb2e6b": {
+      name: "Infrared BGT",
+      symbol: "iBGT",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/ibgt-token-icon.png",
+    },
+    "0xb2f776e9c1c926c4b2e54182fac058da9af0b6a5": {
+      name: "henlo",
+      symbol: "HENLO",
+      decimals: 18,
+      logoURI: "/images/icons/tokens/henlo.png",
+      isPopular: true,
+    },
+    "0xbaadcc2962417c01af99fb2b7c75706b9bd6babe": {
+      name: "Liquid BGT",
+      symbol: "LBGT",
+      decimals: 18,
+      logoURI: "https://www.berapaw.com/static/images/tokens/lbgt.svg",
     },
   },
   validatedFtoAddresses: [],
